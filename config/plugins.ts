6ca44dabe18ca2bb3b59d0f00075337bad4b1351
Nadeem M Siyam <nadeemvadasseril@gmail.com>
2025-02-22 17:53:44 +0530

    updated default limit

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+module.exports = {
+  settings: {
+    "content-api": {
+      enabled: true,
+      config: {
+        defaultLimit: 1000,
+        maxLimit: 1000,
+      },
+    },
+  },
+};
